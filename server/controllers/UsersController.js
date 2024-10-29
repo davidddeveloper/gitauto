bd7224a653327ea0d415625b2fe59fb0fd65bf16
David Conteh <142942999+davidddeveloper@users.noreply.github.com>
2024-10-29 18:56:23 +0000

    change checkToken call in getMe to use the .then instead of callback function

--- server/controllers/UsersController.js
+++ server/controllers/UsersController.js
@@ -7,35 +7,37 @@ const findUser = require('../controllers/findUser');
 const createNew = (req, res) => {
   // new user
   const { body } = req;
-  console.log(body)
+
   const { email, password } = body;
 
   if (!email) return res.status(400).send({ error: 'Missing email' });
 
   if (!password) return res.status(400).send({ error: 'Missing password' });
 
-  findUser(res, { email }).then(user => {
+  return findUser(res, { email }).then(user => {
     if (user && user.email === email) {
       return res.status(400).send({ error: 'Already exist' });
     }
-  });
 
-  const newUser = {
-    email,
-    password: hashPassword(password),
-  }
-
-  return dbClient.client.db('gitauto').collection('users').insertOne(newUser, (err, result) => {
-    if (err) {
-      return res.status(500).send({ error: 'Internal error' });
+    // create the new user
+    const newUser = {
+      email,
+      password: hashPassword(password),
     }
-    return res.status(201).send(
-      {
-        email: result.ops[0].email,
-        id: result.ops[0]._id,
-      },
-    );
+  
+    return dbClient.client.db('gitauto').collection('users').insertOne(newUser)
+    .then(result => {
+      return res.status(201).send(
+        {
+          email: newUser.email,
+          id: result.insertedId,
+        },
+      );
+    }).catch(err => {
+      return res.status(500).send({ error: 'Internal error' })
+    })
   });
+  
 };
 
 const getMe = (req, res) => {
@@ -46,8 +48,12 @@ const getMe = (req, res) => {
   if (!token) return res.status(401).send({error: "Unauthorized"});
 
   return checkToken.get_user_id(res, token).then(user_id => {
-    findUser(res, { _id: new ObjectId(user_id) }).then(user => {
+
+    return findUser(res, { _id: new ObjectId(user_id) }).then(user => {
       return res.status(200).send({ email: user.email, id: user._id });
+    }).catch((err) => {
+      console.log("An unexpected error occoured!")
+      return;
     })
   });
 
