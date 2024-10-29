41d67acb99d82e1a10b2694f740e2df0fbbd60a5
David Conteh <142942999+davidddeveloper@users.noreply.github.com>
2024-10-29 18:27:56 +0000

    create async findUser that checks if user is in database

--- server/controllers/findUser.js
+++ server/controllers/findUser.js
@@ -3,7 +3,7 @@ const dbClient = require('../utils/db')
 async function findUser(res, obj) {
   try {
     const user = await dbClient.client.db('gitauto').collection('users').findOne(obj);
-    // Now user is set, you can use it here or return it.
+
     return user;
   } catch (err) {
     return res.status(500).send({ error: `Internal error${err}` });
