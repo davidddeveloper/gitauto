9162556ca9e277c92eb790dcf6bc6a9d95324039
davidddeveloper <programmingdonebydavid@gmail.com>
2024-11-10 00:45:36 +0000

    updated curlscript with the right example request for tunning a model

--- curl-script.sh
+++ curl-script.sh
@@ -7,7 +7,7 @@
 #curl localhost:5000/connect -H "Authorization: Bearer YmVuMTBAZ21haWwuY29tOnRoZXF1aWNrYnJvd25mb3g="
 
 # fine tuned model
-# curl -X POST localhost:5000/fine-tune-model -d '{"name":gitauto"}' -H "x-token: eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpYXQiOjE3MzAyMTE4NDIsImV4cCI6MTczMjgwMzg0Mn0.q56wPLwilzrU2efdixRQF-OqB2QOarPd82LUlQwxkSE"
+# curl -X POST localhost:5000/fine-tune-model -d '{"name":"gitauto"}' -H "Content-Type: application/json" -H "x-token: eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpYXQiOjE3MzAyMTE4NDIsImV4cCI6MTczMjgwMzg0Mn0.q56wPLwilzrU2efdixRQF-OqB2QOarPd82LUlQwxkSE"
 
 # generate commit
 curl -X POST http://localhost:5000/generate-commit-msg \
